995adf767f1234ee28e660a2590d05e8eb82bd50
beonit <beonit@gmail.com>
2011-12-04 00:29:30 +0900

    codeviewer word split complete

--- assets/www/codeviewer.js
+++ assets/www/codeviewer.js
@@ -2,8 +2,8 @@ var prj;
 var extension;
 function codeviewPreloadHook () {
     // load file
-    // filestr = window.Codeview.loadfile();
-    // $("pre").text( filestr );
+    filestr = window.Codeview.loadfile();
+    $("pre").text( filestr );
 }
 
 function codeviewPostloadHook () {
@@ -16,25 +16,25 @@ function codeviewPostloadHook () {
 
 function addAction() {
     labels = document.getElementsByTagName("code"); 
-    var varRegex = /[-_\w\d]+/ig;
-    var lineOrg = "";
     var line = "";
+    var numOfMatch;
     for( var i = 0; i < labels.length; i++ ){
-        var node = document.createElement('code');
-        lineOrg = labels[i].innerHTML.replace("&nbsp;", " ").replace("&gt;"," ").replace("&lt;"," ");
+        line = labels[i].innerHTML.replace("&nbsp;", " ").replace("&gt;","<").replace("&lt;",">");
         line = line.match(/[-_\d\w]+|[^-_\d\w]*/ig);
+        numOfMatch=0;
         for ( var j in line ) { 
-            
-            if ( line[j].match( /[-_\d\w]+/i ) )
+            if ( line[j].match( /[-_\d\w]+/i ) ){
+                numOfMatch++;
                 line[j] = '<code class="' + labels[i].className + '" onclick="clickHook">' + line[j] + '</code>';
+            }
         }
         replaced = line.join('');
         labels[i].innerHTML = replaced;
-        i += line.length;
+        i += numOfMatch;
     }
 }
 
 function clickHook() {
-    // window.Codeview.Clickhook( this.innerHTML );
-    console.log("onclick");
+    window.Codeview.Clickhook( this.innerHTML );
+    // console.log("onclick");
 }
